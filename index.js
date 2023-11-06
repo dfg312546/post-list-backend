f118024929a6248ac3e781fdcc1a3ecab96e9d30
大魚 <132785492+dfg312546@users.noreply.github.com>
2023-11-06 21:32:45 +0800

    CORS6

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const HttpError = require('./api/models/http-error');
 const app = express();
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST,PATCH, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();

79cabcd1cd922e9ea75cfeb181511cc5814b988b
大魚 <132785492+dfg312546@users.noreply.github.com>
2023-11-06 20:58:22 +0800

    CORS2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./api/routes/post-routes');

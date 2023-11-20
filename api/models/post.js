08094182bc2db8da71e8e2f2794f053f1032e383
大魚 <132785492+dfg312546@users.noreply.github.com>
2023-11-20 14:27:29 +0800

    userpostconnect7

--- api/models/post.js
+++ api/models/post.js
@@ -6,8 +6,8 @@ const postSchema = new Schema({
   title: {type:String, required:true},
   description: {type:String, required:true},
   date: { type: Date },
+  name: {type:String},
   creator: { type: mongoose.Types.ObjectId, required:true ,ref: 'User' },
-  creatorId:{ type: mongoose.Types.ObjectId, required:true ,ref: 'User' }
 });
 
 module.exports = mongoose.model('Post',postSchema)

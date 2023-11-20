a3d6e1829c25a4cc525d9feab4bc00bdaa9ce23a
大魚 <132785492+dfg312546@users.noreply.github.com>
2023-11-20 16:28:53 +0800

    personauth8

--- api/controllers/posts-controllers.js
+++ api/controllers/posts-controllers.js
@@ -168,8 +168,23 @@ const deletePost = async (req, res, next) => {
     return next(error);
   }
 
+  // try {
+  //   await post.deleteOne()
+  // } catch (err) {
+  //   const error = new HttpError(
+  //     'Something went wrong, could not delete post.',
+  //     500
+  //   );
+  //   return next(error);
+  // }
+
   try {
-    await post.deleteOne()
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    post.creator.posts.pull(post);
+    await post.creator.save({ session: sess });
+    await post.deleteOne({ session: sess });
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not delete post.',
@@ -177,21 +192,6 @@ const deletePost = async (req, res, next) => {
     );
     return next(error);
   }
-
-  // try {
-  //   const sess = await mongoose.startSession();
-  //   sess.startTransaction();
-  //   await post.deleteOne({ session: sess });
-  //   post.creator.posts.pull(post);
-  //   await post.creator.save({ session: sess });
-  //   await sess.commitTransaction();
-  // } catch (err) {
-  //   const error = new HttpError(
-  //     'Something went wrong, could not delete place.',
-  //     500
-  //   );
-  //   return next(error);
-  // }
 
   res.status(200).json({ message: 'Deleted post.' });
 }
